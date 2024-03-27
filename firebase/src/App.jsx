658829646b0585c2d16089dbc6cc2a7c1b6fe4f9
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-03-27 11:02:22 +0530

    setup firebase in correct way

---
+++ firebase/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useFirebase } from "./context/Firebase";
+import "./App.css";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const firebase = useFirebase();
+  console.log(firebase);
+
+  return (
+    <>
+      <div>
+        <h1>Firebase</h1>
+        <input
+          type="email"
+          placeholder="Enter your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter your Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={() => firebase.SignupuserwithEmailPassword(email, password)}>Signup</button>
+     </div>
+    </>
+  );
+}
+
+export default App;
